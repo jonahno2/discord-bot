7b76d65abbe9087acf3fecfc4f110136db46ae06
jonahno2 <110586275+jonahno2@users.noreply.github.com>
2022-08-04 01:14:53 -0400

    Create pennies.py

---
+++ pennies.py
@@ -0,0 +1,24 @@
+var j = {
+     name: "Jonah",
+     pennies: 0  
+}
+
+var a = {
+     name: "Aidan",
+     pennies: 0  
+}
+
+var o = {
+     name: "Oliver",
+     pennies: 0  
+}
+
+var h = {
+     name: "Henry",
+     pennies: 0  
+}
+
+var l = {
+     name: "Layla",
+     pennies: 0  
+}
